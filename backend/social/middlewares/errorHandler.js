ab0b3db56728a8f6a3ccd2e835e8764be1fbc294
NaumanShabbir-at-423644851055 <naumanshabbir@ezwage.com>
2023-12-25 18:13:22 +0500

    fixes

--- backend/social/middlewares/errorHandler.js
+++ backend/social/middlewares/errorHandler.js
@@ -1,17 +1,17 @@
-const { RESPONSE_CONSTANTS } = require("../utilities/constants");
+const MyCustomError = require('./myCustomErrorHandler'); // Adjust the path accordingly
 
 const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-  
-    // Handle different types of errors and send appropriate responses
-    if (err instanceof MyCustomError) {
-      res.status(RESPONSE_CONSTANTS.BAD_REQUEST).json({ error: err.message });
-    } else {
-      res.status(RESPONSE_CONSTANTS.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' });
-    }
-  
-    // Ensure to call next with the error parameter
-    next(err);
-  };
-  
-  module.exports=errorHandler
+  console.error(err.stack);
+
+  // Handle different types of errors and send appropriate responses
+  if (err instanceof MyCustomError) {
+    res.status(err.status).json({ error: err.message });
+  } else {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+
+  // Ensure to call next with the error parameter
+  next(err);
+};
+
+module.exports = errorHandler;
